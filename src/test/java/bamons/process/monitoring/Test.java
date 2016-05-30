531fbd54c40e6cfb606a810d3a4958f69aab315a
david100gom <gomkwc@gmail.com>
2016-05-30 16:30:46 +0900

    First Commit

--- src/test/java/bamons/process/monitoring/Test.java
+++ src/test/java/bamons/process/monitoring/Test.java
@@ -1,6 +1,14 @@
 package bamons.process.monitoring;
 
+import org.springframework.util.StringUtils;
+
 public class Test {
 
     // 테스트 파일
+    public static void main(String[] args) throws Exception {
+        String targetDate = "";
+        boolean a = StringUtils.isEmpty(targetDate);
+
+        System.out.print("RESULT : "+a);
+    }
 }
